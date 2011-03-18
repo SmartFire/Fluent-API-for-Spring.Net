b03cd7e0a336513f2e5ce9e3a7c39573a1641d2e
thenapoleon <yoann.esnaud@hotmail.com>
2011-03-18 13:29:04 +0000

    more missing files

---
+++ FluentSpring/Context/Conventions/IConvention.cs
@@ -0,0 +1,9 @@
+﻿using Spring.Objects.Factory.Support;
+
+namespace FluentSpring.Context.Conventions
+{
+    public interface IConvention
+    {
+        void ApplyConvention(string identifierName, IConfigurableObjectDefinition objectDefinition);
+    }
+}
---
+++ FluentSpring/Context/Factories/ConstructorFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Spring.Context;
+
+namespace FluentSpring.Context.Factories
+{
+    public class ConstructorFactory : IApplicationContextAware
+    {
+        private IApplicationContext _applicationContext;
+
+        public IApplicationContext ApplicationContext
+        {
+            set { _applicationContext = value; }
+        }
+
+        public T CreateInstance<T>(Func<IApplicationContext, T> objectConstructor)
+        {
+            return objectConstructor(_applicationContext);
+        }
+    }
+}
